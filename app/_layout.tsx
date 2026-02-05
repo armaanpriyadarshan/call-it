3536eb9d663501d03a3bb201759800752c5d285c
Ethan <ethanpzhou@berkeley.edu>
2026-02-05 12:00:33 -0800

    test, give me contributions

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,7 +9,7 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/use-color-scheme';
 
-// Keep splash screen visible
+// Keep splash screen visible test
 SplashScreen.preventAutoHideAsync();
 
 function RootLayoutNav() {
